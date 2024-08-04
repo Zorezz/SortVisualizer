cefaf50201e83b006cf999e33e06b38c9968631e
Emma Larsson <emma.larsson505@gmail.com>
2024-08-04 21:19:20 +0200

    refactored all code because of stack overflow in old codebase

--- sort.c
+++ sort.c
@@ -1,104 +1,109 @@
-#include <SDL.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <time.h>
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
 
 #define SCREEN_WIDTH 910
 #define SCREEN_HEIGHT 750
 #define ARRAY_LENGTH 130
-
-const int rectSize = 7;
+#define rectSize 7
 
 int arr1[ARRAY_LENGTH];
 int arr2[ARRAY_LENGTH];
 
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-SDL_Surface* screenSurface = NULL;
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
 
 bool complete = false;
 
-int main (int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
-    while(1) {
-        printf("\n");
-        if(controls()) {
-            execute();
-        }
-        else {
-            printf("EXITING PROGRAM\n");
-            break;
-        }
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Could not init SDL: %s\n", SDL_GetError());
+        exit(1);
     }
 
-    return 0;
-}
+    window = SDL_CreateWindow(
+        "Sorting Visualizer",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        910,
+        750,
+        SDL_WINDOW_OPENGL
+    );
 
-// initialize SDL
-int init() {
-    int success = 1;
-
-    if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-        success = 0;
+    if (!window) {
+        printf("Failed to create window: %s\n", SDL_GetError());
+        exit(1);
     }
-    else {
-        window = SDL_CreateWindow( "Sorting Visualizer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );\
-        if ( window == NULL ) {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-            success = 0;
-        }
-        else {
-            renderer = SDL_CreateRenderer(window, -1, 0);
-            if ( renderer == NULL ) {
-                printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
-                success = false;
+
+    renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED
+    );
+    if (!renderer) {
+        printf("Renderer could not be created: %s", SDL_GetError());
+        exit(1);
+    }
+
+    randomizeArray();
+    loadArray();
+    //debugPrintArr(arr2);
+
+    // main loop
+    bool should_quit = false;
+    while (!should_quit) {
+        SDL_Event event;
+        while(SDL_PollEvent(&event)) {
+            switch(event.type) {
+                case SDL_QUIT:
+                    should_quit = true;
+                    break;
+                default:
+                    break;
             }
+            insertSort(arr1, ARRAY_LENGTH);
         }
-    }
-    return success;
+   }
 }
 
-// closing function to kill window and renderer
-void close() {
-    SDL_DestroyRenderer(renderer);
-    renderer = NULL;
+void visualize() {
+            // draw the array
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+            SDL_RenderClear(renderer);
 
-    SDL_DestroyWindow(window);
-    window = NULL;
+            int j = 0;
 
-    SDL_Quit();
+            for (int i = 0; i <= SCREEN_WIDTH - rectSize; i += rectSize) {
+                SDL_PumpEvents();
+
+                SDL_Rect rect = { i, 0, rectSize, arr1[j] };
+                SDL_SetRenderDrawColor(renderer, 170, 183, 184, 0);
+                SDL_RenderDrawRect(renderer, &rect);
+                j++;
+            }
+            SDL_RenderPresent(renderer);
 }
 
-void visualize(int x, int y, int z) {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-    SDL_RenderClear(renderer);
+void loadArray() {
+    memcpy(arr1, arr2, sizeof(int) * ARRAY_LENGTH);
+}
 
-    int j = 0;
-
-    for (int i = 0; i <= SCREEN_WIDTH - rectSize; i += rectSize){
-        SDL_PumpEvents();
-
-        SDL_Rect rect = { i, 0, rectSize, arr1[j] };
-        if (complete) {
-            SDL_SetRenderDrawColor(renderer, 100, 180, 100, 0);
-            SDL_RenderDrawRect(renderer, &rect);
-        }
-        else if (j == x || j == z) {
-            SDL_SetRenderDrawColor(renderer, 100, 180, 100, 0);
-            SDL_RenderFillRect(renderer, &rect);
-        }
-        else if (j == y) {
-            SDL_SetRenderDrawColor(renderer, 165, 105, 189, 0);
-            SDL_RenderFillRect(renderer, &rect);
-        }
-        else {
-            SDL_SetRenderDrawColor(renderer, 170, 183, 184, 0);
-            SDL_RenderDrawRect(renderer, &rect);
-        }
-        j++;
+void randomizeArray() {
+    unsigned int seed = (unsigned)time(NULL);
+    srand(seed);
+    for (int i = 0; i < ARRAY_LENGTH; i++) {
+        int randNum = rand() % (SCREEN_HEIGHT);
+        arr2[i] = randNum;
     }
-    SDL_RenderPresent(renderer);
+}
+
+void debugPrintArr(int arr[]) {
+    for (int i = 0; i < ARRAY_LENGTH; i++) {
+        printf("%d ", arr[i]);
+    }
 }
 
 void insertSort(int arr[], int n) {
@@ -110,86 +115,12 @@ void insertSort(int arr[], int n) {
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;
+
+            visualize();
+            SDL_Delay(5);
         }
         arr[j + 1] = key;
     }
 }
 
-void execute() {
-    if(!init()) {
-        printf("SDL Initialization Failed\n");
-    }
-    else {
-        randomizeArray();
-        loadArray();
 
-        SDL_Event e;
-        int quit = false;
-        while (!quit) {
-            while (SDL_PollEvent(&e) != 0)
-            {
-                if (e.type == SDL_QUIT)
-                {
-                    quit = 1;
-                    complete = false;
-                }
-                else if(e.type == SDL_KEYDOWN) {
-                    switch(e.key.keysym.sym) {
-                        case(SDLK_q):
-                            quit = 1;
-                            complete = false;
-                            printf("EXITING SORTING VISUALIZER\n");
-                            break;
-                        case(SDLK_0):
-                            randomizeArray();
-                            complete = false;
-                            loadArray();
-                            printf("NEW RANDOM LIST GENERATED\n");
-                            break;
-                    }
-                }
-            }
-            visualize(-1, -1, -1);
-        }
-        close();
-    }
-}
-
-void loadArray() {
-    memcpy(arr1, arr2, sizeof(int)*ARRAY_LENGTH);
-}
-
-void randomizeArray() {
-    unsigned int seed = (unsigned)time(NULL);
-    srand(seed);
-    for (int i = 0; i < ARRAY_LENGTH; i++) {
-        int randNum = rand()%(SCREEN_HEIGHT);
-        arr2[i] = randNum;
-    }
-}
-
-int controls() {
-    printf(
-        "WARNING: Giving repetitive commands may cause latency and the visualizer may behave unexpectedly. Please give a new command only after the current command's execution is done.\n\n"
-        "Available Controls inside Sorting Visualizer:-\n"
-        "    Use 0 to Generate a different randomized list.\n"
-        "    Use 1 to start Selection Sort Algorithm.\n"
-        "    Use 2 to start Insertion Sort Algorithm.\n"
-        "    Use 3 to start Bubble Sort Algorithm.\n"
-        "    Use 4 to start Merge Sort Algorithm.\n"
-        "    Use 5 to start Quick Sort Algorithm.\n"
-        "    Use 6 to start Heap Sort Algorithm.\n"
-        "    Use q to exit out of Sorting Visualizer\n\n"
-        "PRESS ENTER TO START SORTING VISUALIZER...\n\n"
-        "Or type -1 and press ENTER to quit the program."
-    );
-
-    int input;
-    scanf("%d", &input);
-
-    if(input == -1) {
-        return 0;
-    }
-
-    return 1;
-}
